b4bc66878a4e8ce1502ae91569e80c69a5080c2c
Imran Khan <imran.lko@outlook.com>
2016-06-08 23:42:28 +0530

    Adding Request Logging Middleware

---
+++ Middleware/RequestLogger.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace AspnetCoreRC2Poc.Middleware
+{
+
+    public class RequestLoggerMiddleware{
+
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+         public RequestLoggerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestLoggerMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation("Handling request: " + context.Request.Path);
+            await _next.Invoke(context);
+            _logger.LogInformation("Finished handling request.");
+        }
+
+
+    }
+
+    public static class RequestLoggerExtensions
+{
+    public static IApplicationBuilder UseRequestLogger(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<RequestLoggerMiddleware>();
+    }
+}
+    
+}
+
